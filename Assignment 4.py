d68e8e06a1aa4575c92a7c599ee43ec6dc1daaa4
Resh-j <misakitakume90@gmail.com>
2024-02-18 20:14:37 +0100

    new

---
+++ Assignment 4.py
@@ -0,0 +1,50 @@
+#Assignment 4.1
+'''
+pizza_list = ['cheeseburst pizza', 'tandoori pizza', 'chiken pizza']
+for pizza in pizza_list:
+  print(pizza)
+
+pizza_list = ['cheeseburst pizza', 'tandoori pizza', 'chiken pizza']
+for pizza in pizza_list:
+    message = ('I like')
+    print(message, pizza)
+
+pizza_list = ['cheeseburst', 'tandoori', 'chiken']
+for pizza in pizza_list:
+    message = ('I like')
+    print(message, pizza)
+print('I really love tandoori pizza')
+
+'''
+#Assignment 4.2
+
+#animal_list = ['Lion', 'Panthor', 'Cat']
+#for animal in animal_list:
+#    print(animal)
+
+#animal_list = ['Lion', 'Panthor', 'Cat']
+#for animal in animal_list:
+#    message = ('is a member of Cat family')
+#   print(animal, message)
+
+'''
+animal_list = ['Lion', 'Panthor', 'Cat']
+for animal in animal_list:
+    message = ('is a member of Cat family')
+    print(animal, message)
+print('All of them have whiskers')
+'''
+
+#Assignment 4.3
+#for values in range (1,21):
+#    print(values)
+
+#Assignment 4.4
+#for values in range (1, 1000001):
+#    print(values)
+
+#Assignment 4.5
+list = []
+for i in range (1, 1000001):
+    print (min(list))
+  
